daa2737b54dde19f3e148b6d30e994905d9608b2
Anjali <anjalidilrangi@gmail.com>
2023-05-10 23:30:13 +0530

    Initial Commit

---
+++ ios_cw2/ios_cw2App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw2App.swift
+//  ios_cw2
+//
+//  Created by Test on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
